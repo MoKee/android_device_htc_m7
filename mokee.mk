9a1dce59d68c6caeef51416c2415fa13748d0ad9
martincz <martincz@mokeedev.com>
2017-01-29 20:26:28 +0800

    m7: mokee-ify

--- mokee.mk
+++ mokee.mk
@@ -15,11 +15,11 @@
 
 $(call inherit-product, device/htc/m7/full_m7.mk)
 
-# Inherit Lineage full phone configuration
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit MK full phone configuration
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Device naming
-PRODUCT_NAME := lineage_m7
+PRODUCT_NAME := mk_m7
 
 # Override build props
 PRODUCT_BUILD_PROP_OVERRIDES += \
